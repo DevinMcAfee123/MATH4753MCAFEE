d5152f14d4d1e7c6515d98ba5785ba0743bf369e
Devin McAfee <devin.m.mcafee-1@ou.edu>
2021-03-26 18:31:20 -0500

    Added fire dataset to package

---
+++ R/fire.r
@@ -0,0 +1,13 @@
+#' Fire data set.
+#'
+#' A data set showing the distance of fires from nearest fire station and the cost of their damage.
+#'
+#' @format A data frame with 15 rows and 2 variables
+#' \describe {
+#'   \item{DISTANCE}{distance in miles from nearest fire station}
+#'   \item{Damage}{damage caused by fire in thousands of dollars}
+#'   ...
+#' }
+#' @source \url{https://bookshelf.vitalsource.com/#/books/9781498728881}
+"fire"
+
